50d04d7d011944f604091e7f077d8cae797a355f
Khang <nldkhang@gmail.com>
2026-09-18 14:27:18 +0700

    feat: save user info in redux when login

--- fe/src/state/profile/profileSlice.ts
+++ fe/src/state/profile/profileSlice.ts
@@ -1,15 +1,21 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 interface profileState {
+  id: number;
+  cartQuantity: number;
   fullName: string;
-  address: string;
   phoneNumber: string;
+  address: string;
+  role: "admin" | "user";
 }
 
 const initialState: profileState = {
+  id: 0,
+  cartQuantity: 0,
   fullName: "",
   address: "",
   phoneNumber: "",
+  role: "user",
 };
 
 const profileSlice = createSlice({
