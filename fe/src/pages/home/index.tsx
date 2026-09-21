5232660174e6556c45898b18cec3e7cb1ca64117
Khang <nldkhang@gmail.com>
2026-09-21 08:51:32 +0700

    feat: modify home and service pages for auto refresh feature test

--- fe/src/pages/home/index.tsx
+++ fe/src/pages/home/index.tsx
@@ -1,3 +1,20 @@
+import { handleLogout } from "@/lib/handleLogou";
+import { useAppSelector } from "@/state/hooks";
+import { useNavigate } from "react-router";
+
 export default function Page() {
-  return <h1>This is home page</h1>;
+  const token = useAppSelector((state) => state.token);
+  const navigate = useNavigate();
+  return (
+    <div className="flex gap-3">
+      <h1>This is home page</h1>
+      {token.accessToken !== "" && (
+        <button onClick={() => handleLogout()}>Logout</button>
+      )}
+      <button onClick={() => navigate("/service")}>To service page</button>
+      <button onClick={() => navigate("/product/1")}>
+        To Product Detail page
+      </button>
+    </div>
+  );
 }
--- fe/src/pages/service/index.tsx
+++ fe/src/pages/service/index.tsx
@@ -1,3 +1,17 @@
+import { handleLogout } from "@/lib/handleLogou";
+import { useAppSelector } from "@/state/hooks";
+import { useNavigate } from "react-router";
+
 export default function Page() {
-  return <h1>This is Service Page</h1>;
+  const token = useAppSelector((state) => state.token);
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>This is Service Page</h1>
+      {token.accessToken !== "" && (
+        <button onClick={() => handleLogout()}>Logout</button>
+      )}
+      <button onClick={() => navigate("/home")}>To home page</button>
+    </div>
+  );
 }
