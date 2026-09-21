9b3567e4c8aa5e51bf59a20a1f0b9922fa2bd377
Khang <nldkhang@gmail.com>
2026-09-21 11:39:50 +0700

    feat: add toaster in root

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -5,16 +5,18 @@ import { router } from "@/router";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/providers/QueryProvider";
-
 import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
+import { Toaster } from "sonner";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <TooltipProvider>
           <RouterProvider router={router} />
+          <Toaster />
         </TooltipProvider>
       </Provider>
     </QueryClientProvider>
