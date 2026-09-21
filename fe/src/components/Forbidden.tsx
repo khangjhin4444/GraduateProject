bad5a82ae554f96268247e9ef83e2e9a43cef49a
Khang <nldkhang@gmail.com>
2026-09-21 15:05:49 +0700

    feat: implement forbidden page

---
+++ fe/src/components/Forbidden.tsx
@@ -0,0 +1,7 @@
+export default function ForbiddenPage() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center bg-black">
+      <h1 className="text-2xl text-white font-bold">403 Forbidden</h1>
+    </div>
+  );
+}
