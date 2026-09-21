8562cd1f57e0f57c85c9289b6bc395ed400f1a62
Khang <nldkhang@gmail.com>
2026-09-21 11:11:16 +0700

    refactor: move pages that has header in to a group

---
+++ fe/src/pages/HasHeader/service/index.tsx
@@ -0,0 +1,17 @@
+import { handleLogout } from "@/lib/handleLogout";
+import { useAppSelector } from "@/state/hooks";
+import { useNavigate } from "react-router";
+
+export default function Page() {
+  const token = useAppSelector((state) => state.token);
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>This is Service Page</h1>
+      {token.accessToken !== "" && (
+        <button onClick={() => handleLogout()}>Logout</button>
+      )}
+      <button onClick={() => navigate("/home")}>To home page</button>
+    </div>
+  );
+}
--- fe/src/pages/contact/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>This is Contact Page</h1>;
-}
