fec89edf5c79e30bed0948a5a86bd9d58302162e
Khang <nldkhang@gmail.com>
2026-09-22 16:43:59 +0700

    chore: add login page title

--- fe/src/pages/auth/login/index.tsx
+++ fe/src/pages/auth/login/index.tsx
@@ -72,112 +72,115 @@ export default function Page() {
     }
   }
   return (
-    <Card className="w-full md:w-2/3 lg:w-3/4 shadow-2xl py-10 px-5 rounded-[60px]">
-      <CardHeader>
-        <CardTitle className="text-2xl bg-linear-to-r from-teal-400 to-blue-500 bg-clip-text text-transparent">
-          Login Account
-        </CardTitle>
-        <CardDescription>Please enter your account</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={form.handleSubmit(onSubmit)} id="register-form">
-          <FieldGroup>
-            <Controller
-              name="username"
-              control={form.control}
-              render={({ field, fieldState }) => {
-                return (
-                  <Field
-                    className="w-full mt-3"
-                    data-invalid={fieldState.invalid}
-                  >
-                    <FieldLabel htmlFor="username-input">
-                      Your Username
-                    </FieldLabel>
-                    <div className="px-2">
-                      <Input
-                        {...field}
-                        aria-invalid={fieldState.invalid}
-                        id="username-input"
-                        placeholder="Username"
-                      />
-                    </div>
-                    {fieldState.error && (
-                      <p className="text-[12px] text-red-500 font-semibold ml-3 pt-2">
-                        {fieldState.error.message}
-                      </p>
-                    )}
-                  </Field>
-                );
-              }}
-            />
-            <Controller
-              name="password"
-              control={form.control}
-              render={({ field, fieldState }) => {
-                return (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="password-input">Password</FieldLabel>
-                    <div className="px-2">
-                      <InputGroup>
-                        <InputGroupInput
-                          aria-invalid={fieldState.invalid}
+    <>
+      <title>JK Keyboard - Login</title>
+      <Card className="w-full md:w-2/3 lg:w-3/4 shadow-2xl py-10 px-5 rounded-[60px]">
+        <CardHeader>
+          <CardTitle className="text-2xl bg-linear-to-r from-teal-400 to-blue-500 bg-clip-text text-transparent">
+            Login Account
+          </CardTitle>
+          <CardDescription>Please enter your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)} id="register-form">
+            <FieldGroup>
+              <Controller
+                name="username"
+                control={form.control}
+                render={({ field, fieldState }) => {
+                  return (
+                    <Field
+                      className="w-full mt-3"
+                      data-invalid={fieldState.invalid}
+                    >
+                      <FieldLabel htmlFor="username-input">
+                        Your Username
+                      </FieldLabel>
+                      <div className="px-2">
+                        <Input
                           {...field}
-                          id="password-input"
-                          type={!showPassword ? "password" : "text"}
-                          onChange={(e) => {
-                            field.onChange(e);
-                            if (fieldState.invalid) {
-                              form.clearErrors(field.name);
-                            }
-                          }}
-                          placeholder="Password"
+                          aria-invalid={fieldState.invalid}
+                          id="username-input"
+                          placeholder="Username"
                         />
-                        <InputGroupAddon
-                          align={"inline-end"}
-                          className="cursor-pointer"
-                          onClick={() => setShowPassword((prev) => !prev)}
-                        >
-                          {!showPassword ? <EyeIcon /> : <EyeOffIcon />}
-                        </InputGroupAddon>
-                      </InputGroup>
+                      </div>
                       {fieldState.error && (
-                        <p className="text-[12px] text-red-500 font-semibold ml-1 pt-2">
+                        <p className="text-[12px] text-red-500 font-semibold ml-3 pt-2">
                           {fieldState.error.message}
                         </p>
                       )}
-                    </div>
-                  </Field>
-                );
-              }}
-            />
-          </FieldGroup>
-        </form>
-        <Field className="mt-8">
-          <Button
-            type="submit"
-            disabled={form.formState.isSubmitting}
-            form="register-form"
-            className="py-6 rounded-xl bg-linear-to-r from-teal-400 to-blue-500 cursor-pointer"
-          >
-            {!form.formState.isSubmitting ? "Login" : "Logging in..."}
-          </Button>
-        </Field>
-        <div className="flex justify-center items-center gap-2 mt-5">
-          <p>Don't have an account?</p>
-          <a
-            href="/register"
-            className="text-blue-500 border-b-2 border-blue-500 font-bold"
-          >
-            Register Here!
-          </a>
-        </div>
-      </CardContent>
-      {form.formState.errors.root && (
-        <div className="flex justify-center text-red-500 font-bold">
-          {form.formState.errors.root.message}
-        </div>
-      )}
-    </Card>
+                    </Field>
+                  );
+                }}
+              />
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => {
+                  return (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor="password-input">Password</FieldLabel>
+                      <div className="px-2">
+                        <InputGroup>
+                          <InputGroupInput
+                            aria-invalid={fieldState.invalid}
+                            {...field}
+                            id="password-input"
+                            type={!showPassword ? "password" : "text"}
+                            onChange={(e) => {
+                              field.onChange(e);
+                              if (fieldState.invalid) {
+                                form.clearErrors(field.name);
+                              }
+                            }}
+                            placeholder="Password"
+                          />
+                          <InputGroupAddon
+                            align={"inline-end"}
+                            className="cursor-pointer"
+                            onClick={() => setShowPassword((prev) => !prev)}
+                          >
+                            {!showPassword ? <EyeIcon /> : <EyeOffIcon />}
+                          </InputGroupAddon>
+                        </InputGroup>
+                        {fieldState.error && (
+                          <p className="text-[12px] text-red-500 font-semibold ml-1 pt-2">
+                            {fieldState.error.message}
+                          </p>
+                        )}
+                      </div>
+                    </Field>
+                  );
+                }}
+              />
+            </FieldGroup>
+          </form>
+          <Field className="mt-8">
+            <Button
+              type="submit"
+              disabled={form.formState.isSubmitting}
+              form="register-form"
+              className="py-6 rounded-xl bg-linear-to-r from-teal-400 to-blue-500 cursor-pointer"
+            >
+              {!form.formState.isSubmitting ? "Login" : "Logging in..."}
+            </Button>
+          </Field>
+          <div className="flex justify-center items-center gap-2 mt-5">
+            <p>Don't have an account?</p>
+            <a
+              href="/register"
+              className="text-blue-500 border-b-2 border-blue-500 font-bold"
+            >
+              Register Here!
+            </a>
+          </div>
+        </CardContent>
+        {form.formState.errors.root && (
+          <div className="flex justify-center text-red-500 font-bold">
+            {form.formState.errors.root.message}
+          </div>
+        )}
+      </Card>
+    </>
   );
 }
