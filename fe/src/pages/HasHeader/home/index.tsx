beb6a79baed129d75cbcd503d12d92d02f5862cc
Khang <nldkhang@gmail.com>
2026-09-22 08:56:44 +0700

    feat(homepage): add carosel section

--- fe/src/pages/HasHeader/home/index.tsx
+++ fe/src/pages/HasHeader/home/index.tsx
@@ -1,14 +1,20 @@
 import { handleLogout } from "@/lib/handleLogout";
 import { useAppSelector } from "@/state/hooks";
 import { useNavigate } from "react-router";
+import Carosel from "./_components/Carosel";
 
 export default function Page() {
   const token = useAppSelector((state) => state.token);
   const navigate = useNavigate();
   return (
-    <div className="flex gap-3">
+    <div className="flex gap-3 flex-col px-4 w-full">
       <title>JK Keyboard-Home Page</title>
-      <h1>This is home page</h1>
+      <Carosel></Carosel>
+
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-2xl font-semibold text-foreground">In stock</h2>
+      </div>
+
       {token.accessToken !== "" && (
         <button onClick={() => handleLogout()}>Logout</button>
       )}
