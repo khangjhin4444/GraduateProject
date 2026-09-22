d159e6fd572f040834c965444d39d7523a1fe7c4
Khang <nldkhang@gmail.com>
2026-09-22 14:51:56 +0700

    feat: implement skeleton component when fetching data

--- fe/src/pages/HasHeader/home/index.tsx
+++ fe/src/pages/HasHeader/home/index.tsx
@@ -1,31 +1,18 @@
-import { handleLogout } from "@/lib/handleLogout";
-import { useAppSelector } from "@/state/hooks";
-import { useNavigate } from "react-router";
 import Carosel from "./_components/Carosel";
 import KeyboardSection from "./_components/KeyboardSection";
 import SwitchKeycapSection from "./_components/SwitchKeycapSection";
 
 export default function Page() {
-  const token = useAppSelector((state) => state.token);
-  const navigate = useNavigate();
   return (
     <div className="flex gap-3 flex-col px-4 w-full">
       <title>JK Keyboard-Home Page</title>
       <Carosel></Carosel>
 
-      <div className="flex flex-col justify-center items-center w-full">
+      <div className="flex flex-col justify-center items-center w-full pb-10">
         <h2 className="text-2xl font-semibold text-foreground">In stock</h2>
         <KeyboardSection></KeyboardSection>
         <SwitchKeycapSection></SwitchKeycapSection>
       </div>
-
-      {token.accessToken !== "" && (
-        <button onClick={() => handleLogout()}>Logout</button>
-      )}
-      <button onClick={() => navigate("/service")}>To service page</button>
-      <button onClick={() => navigate("/product/1")}>
-        To Product Detail page
-      </button>
     </div>
   );
 }
